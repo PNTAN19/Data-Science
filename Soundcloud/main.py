7aa4b7d60a8950ee62cb73260ed0c614894211ec
Viru <minhnhatnan@gmail.com>
2021-11-03 20:26:30 +0700

    playlist

--- Soundcloud/main.py
+++ Soundcloud/main.py
@@ -1,10 +1,80 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import requests
+import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+import pandas as pd
+
+def getTracks(playlist_url):
+    driver = webdriver.Chrome()
+    driver.get(playlist_url)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+
+    user_name = soup.find("a", {"class": "userBadge__usernameLink"}).getText().strip()
+    playlist_title = soup.find("h1", {"class": "soundTitle__title"}).getText().strip()
+    track_items = soup.find_all("div", {"class": "trackItem__content"})
+
+    track_list = [];
+
+    for track_item in track_items:
+        track_name = track_item.getText();
+        track_list.append(track_name.strip().replace("\n", " "))
+
+    track_str = []
+    for track_item in track_list:
+        track_str.append(track_item)
+    track_str = ','.join(track_str)
+    if track_str == '':
+        track_str = 'None'
+
+    return user_name, playlist_title, track_str
+
+def getPlaylistURLs(user_playlist_url):
+    url = user_playlist_url
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(2)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    contents = soup.find_all("a", {"class": "sound__coverArt"})
+    urls = []
+
+    for content in contents:
+        urls.append('https://soundcloud.com' + content['href'])
+
+    return urls
+
+def getPlaylists(user_playlist_urls):
+    playlist_lst = []
+
+    for user_url in user_playlist_urls:
+        playlist_urls = getPlaylistURLs(user_url + '/sets')
+        for playlist_url in playlist_urls:
+            playlist_lst.append(getTracks(playlist_url))
+
+    return playlist_lst
+
 
 my_url = "https://soundcloud.com/discover"
 driver = webdriver.Chrome()
@@ -53,29 +123,43 @@ for user_url in set_link:
     for follower in followers:
         if follower not in set_link:
             set_link.append("https://soundcloud.com" + follower.get("href"))
+    break
 
-for item in set_user:
-    temp = item[1] + "/tracks"
-    driver.get(temp)
-    while (True):
-        try:
-            myElem = WebDriverWait(driver, 30).until(
-                EC.presence_of_element_located((By.CLASS_NAME, 'profileHeaderInfo__userName')))
-            if myElem is not None:
-                break
-        except TimeoutException:
-            break;
+# for item in set_user:
+#     temp = item[1] + "/tracks"
+#     driver.get(temp)
+#     while (True):
+#         try:
+#             myElem = WebDriverWait(driver, 30).until(
+#                 EC.presence_of_element_located((By.CLASS_NAME, 'profileHeaderInfo__userName')))
+#             if myElem is not None:
+#                 break
+#         except TimeoutException:
+#             break;
+#
+#     html = driver.page_source
+#     soup = BeautifulSoup(html, "html.parser")
+#     emptyTrack = soup.find("h3", {"class": "sc-type-large emptyNetworkPage__headline"})
+#     if emptyTrack is None:
+#         Track_list = soup.find_all("a", {"class": "sc-link-primary soundTitle__title sc-link-dark sc-text-h4"})
+#         string_track = ""
+#         for track in Track_list:
+#             string_track = string_track + track.getText().strip() + "\t"
+#         k = [p.getText(strip=True) for p in soup.find("h2", {"class": "profileHeaderInfo__userName"})]
+#         print(k[0] + ':' + '\t' + string_track)
+#     else:
+#         k = [p.getText(strip=True) for p in soup.find("h2", {"class": "profileHeaderInfo__userName"})]
+#         print(k[0] + ':' + '\t' + "None")
 
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-    emptyTrack = soup.find("h3", {"class": "sc-type-large emptyNetworkPage__headline"})
-    if emptyTrack is None:
-        Track_list = soup.find_all("a", {"class": "sc-link-primary soundTitle__title sc-link-dark sc-text-h4"})
-        string_track = ""
-        for track in Track_list:
-            string_track = string_track + track.getText().strip() + "\t"
-        k = [p.getText(strip=True) for p in soup.find("h2", {"class": "profileHeaderInfo__userName"})]
-        print(k[0] + ':' + '\t' + string_track)
-    else:
-        k = [p.getText(strip=True) for p in soup.find("h2", {"class": "profileHeaderInfo__userName"})]
-        print(k[0] + ':' + '\t' + "None")
+url_lst = [];
+for i in set_user:
+    url_lst.append(i[1])
+
+print(url_lst)
+
+playlist = getPlaylists(url_lst)
+for item in playlist:
+    print(item)
+
+f_out = pd.DataFrame(playlist, columns=['User', 'Playlist', 'Track'])
+f_out.to_csv('playlist.csv', index=False, sep='\t')
