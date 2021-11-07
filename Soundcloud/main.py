c4695edf148163802f65c176e55678056f143a55
yingyong26072001 <hadung19@clc.fitus.edu.vn>
2021-11-07 14:55:18 +0700

    Update main.py

--- Soundcloud/main.py
+++ Soundcloud/main.py
@@ -1,3 +1,6 @@
+import json
+
+import requests
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
@@ -5,9 +8,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
-#import pandas as pd
+# import pandas as pd
+from selenium.webdriver.common.by import By
 
-#wait
+
+# wait
 def wait(driver, selector_name):
     while (True):
         try:
@@ -16,7 +21,9 @@ def wait(driver, selector_name):
                 break
         except TimeoutException:
             break
-#scroll
+
+
+# scroll
 def scroll(driver):
     # Get scroll height
     last_height = driver.execute_script("return document.body.scrollHeight")
@@ -34,7 +41,8 @@ def scroll(driver):
             break
         last_height = new_height
 
-def getTracks(playlist_url):
+
+def getTracksFromPlaylists(playlist_url):
     driver = webdriver.Chrome()
     driver.get(playlist_url)
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -61,6 +69,7 @@ def getTracks(playlist_url):
 
     return user_name, playlist_title, track_str
 
+
 def getPlaylistURLs(user_playlist_url):
     url = user_playlist_url
     driver = webdriver.Chrome()
@@ -81,93 +90,131 @@ def getPlaylistURLs(user_playlist_url):
 
     return urls
 
+
 def getPlaylists(user_playlist_urls):
     playlist_lst = []
 
     for user_url in user_playlist_urls:
         playlist_urls = getPlaylistURLs(user_url + '/sets')
         for playlist_url in playlist_urls:
-            playlist_lst.append(getTracks(playlist_url))
+            playlist_lst.append(getTracksFromPlaylists(playlist_url))
 
     return playlist_lst
 
 
-my_url = "https://soundcloud.com/discover"
-driver = webdriver.Chrome()
-driver.get(my_url)
-html = driver.page_source
-soup = BeautifulSoup(html, "html.parser")
-link = soup.find_all("a", {
-    "class": "sc-link-secondary sc-link-light playableTile__usernameHeading audibleTile__usernameHeading sc-truncate sc-text-h4"})
+def getTracks(set_user):
+    driver = webdriver.Chrome()
+    set_tracks = set()
+    set_link_tracks = set()
+    for item in set_user:
+        temp = item[2] + "/tracks"
+        driver.get(temp)
 
-set_link = []
-set_user = []
-for item in link:
-    user_url = "https://soundcloud.com" + item.get("href")
-    if user_url not in set_link:
-        set_link.append(user_url)
+        wait(driver, selector_name='sc-link-primary soundTitle__title sc-link-dark sc-text-h4')
 
-for user_url in set_link:
-    if len(set_user) > 1:
-        break;
-    driver.get(user_url)
-    while (True):
-        myElem = WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'profileHeaderInfo__userName')))
-        if myElem is not None:
-            break;
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-    user_name = soup.find("h2", {"class": "profileHeaderInfo__userName"})
-    k = [p.getText(strip=True) for p in user_name]
-    set_user.append([k[0], user_url])
+        scroll(driver)
 
-    # Get followers
-    follower_url = user_url + "/following"
-    driver.get(follower_url)
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        link_tracks = soup.find_all('a', {'class': 'sc-link-primary soundTitle__title sc-link-dark sc-text-h4'})
 
-    #Wait
-    wait(driver, 'userBadgeListItem__heading')
+        for link in link_tracks:
+            string_temp = 'https://soundcloud.com'
+            string_temp = string_temp + link['href']
+            set_link_tracks.add(string_temp)
+        string_id_tracks = ''
+        for link in set_link_tracks:
+            driver.get(link)
+            soup = BeautifulSoup(driver.page_source, "html.parser")
+            string_id_tracks = string_id_tracks + soup.find('meta', {'property': 'twitter:app:url:googleplay'})['content'].split(':')[-1] + ','
 
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-    followers = soup.find_all("a", {"class": "userBadgeListItem__heading"})
-    for follower in followers:
-        if follower not in set_link:
-            set_link.append("https://soundcloud.com" + follower.get("href"))
+        tup = (item[0], string_id_tracks)
+        set_tracks.add(tup)
 
-# for item in set_user:
-#     temp = item[1] + "/tracks"
-#     driver.get(temp)
+    return set_tracks
+
+
+def getTracksAPI(set_user):
+    set_tracks = set()
+    temp1 = 'https://api-v2.soundcloud.com/users/'
+    temp2 = '/tracks?client_id=nGKlrpy2IotLQ0QGwBOmIgSFayis6H4e&limit=100'
+
+    for item in set_user:
+        string_tracks = ''
+        api = temp1 + item[0] + temp2
+        while True:
+            r = requests.get(api)
+            if r.status_code != 200:
+                time.sleep(5.0)
+            else:
+                break
+        for temp in r.json()['collection']:
+            string_tracks = string_tracks + str(temp['id']) + ','
+        tup = (item[0], string_tracks)
+        set_tracks.add(tup)
+    return set_tracks
+
+
+# my_url = "https://soundcloud.com/discover"
+# driver = webdriver.Chrome()
+# driver.get(my_url)
+# html = driver.page_source
+# soup = BeautifulSoup(html, "html.parser")
+# link = soup.find_all("a", {
+#     "class": "sc-link-secondary sc-link-light playableTile__usernameHeading audibleTile__usernameHeading sc-truncate sc-text-h4"})
+#
+# set_link = []
+# set_user = []
+# for item in link:
+#     user_url = "https://soundcloud.com" + item.get("href")
+#     if user_url not in set_link:
+#         set_link.append(user_url)
+#
+# for user_url in set_link:
+#     if len(set_user) > 1:
+#         break;
+#     driver.get(user_url)
 #     while (True):
-#         try:
-#             myElem = WebDriverWait(driver, 30).until(
-#                 EC.presence_of_element_located((By.CLASS_NAME, 'profileHeaderInfo__userName')))
-#             if myElem is not None:
-#                 break
-#         except TimeoutException:
+#         myElem = WebDriverWait(driver, 30).until(
+#             EC.presence_of_element_located((By.CLASS_NAME, 'profileHeaderInfo__userName')))
+#         if myElem is not None:
 #             break;
+#     html = driver.page_source
+#     soup = BeautifulSoup(html, "html.parser")
+#     user_name = soup.find("h2", {"class": "profileHeaderInfo__userName"})
+#     k = [p.getText(strip=True) for p in user_name]
+#     set_user.append([k[0], user_url])
+#
+#     # Get followers
+#     follower_url = user_url + "/following"
+#     driver.get(follower_url)
+#
+#     #Wait
+#     wait(driver, 'userBadgeListItem__heading')
 #
 #     html = driver.page_source
 #     soup = BeautifulSoup(html, "html.parser")
-#     emptyTrack = soup.find("h3", {"class": "sc-type-large emptyNetworkPage__headline"})
-#     if emptyTrack is None:
-#         Track_list = soup.find_all("a", {"class": "sc-link-primary soundTitle__title sc-link-dark sc-text-h4"})
-#         string_track = ""
-#         for track in Track_list:
-#             string_track = string_track + track.getText().strip() + "\t"
-#         k = [p.getText(strip=True) for p in soup.find("h2", {"class": "profileHeaderInfo__userName"})]
-#         print(k[0] + ':' + '\t' + string_track)
-#     else:
-#         k = [p.getText(strip=True) for p in soup.find("h2", {"class": "profileHeaderInfo__userName"})]
-#         print(k[0] + ':' + '\t' + "None")
-
-url_lst = [];
-for i in set_user:
-    url_lst.append(i[1])
-
-playlist = getPlaylists(url_lst)
-for url in playlist:
-    print(url)
-#f_out = pd.DataFrame(playlist, columns=['User', 'Playlist', 'Track'])
-#f_out.to_csv('playlist.csv', index=False, sep='\t')
+#     followers = soup.find_all("a", {"class": "userBadgeListItem__heading"})
+#     for follower in followers:
+#         if follower not in set_link:
+#             set_link.append("https://soundcloud.com" + follower.get("href"))
+#
+#
+#
+# url_lst = [];
+# for i in set_user:
+#     url_lst.append(i[1])
+#
+# playlist = getPlaylists(url_lst)
+# for url in playlist:
+#     print(url)
+# f_out = pd.DataFrame(playlist, columns=['User', 'Playlist', 'Track'])
+# f_out.to_csv('playlist.csv', index=False, sep='\t')
+# tup = ('144468830', '...', 'https://soundcloud.com/pkvpro123')
+# set_user = set()
+# set_user.add(tup)
+# print(getTracksAPI(set_user))
+# getTracksAPI(set_user)
+# print(getTracks(set_user))
+# print(getTracks(set_user))
+# getTracks(set_user)
